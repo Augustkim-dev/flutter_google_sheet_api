448f8bb7270af88f2c1e5247adfbd582d0fd4b2a
Ki Jun Kim <augustkim.dev@gmail.com>
2024-01-17 16:46:22 +0900

    repository 인터페이스로 작성 시작

--- lib/repository/list_data_repository.dart
+++ lib/repository/list_data_repository.dart
@@ -1,6 +1,14 @@
+// api 호출 부분
+// common에서 데이터 연결 호출 미리 해두고 결과 받아서 통신 호출
+// request 보내고 responde 받아서 처리
+
 class ListDataRepository {
   Future<String> test_calculation = Future<String>.delayed(
     const Duration(seconds: 2),
-    () => 'List Data Checked',
+    () {
+      return 'List Data Checked';
+    },
   );
+
+  Future<void> getConnect() async {}
 }
